e14fa1f0f467e443e8636be4a1eabc4f59b93e6d
almostJohn <garcia.johngale@gmail.com>
2022-09-09 14:39:25 +0800

    initial commit

---
+++ src/script.js
@@ -0,0 +1,38 @@
+const display = document.querySelector("#display");
+
+const buttons = document.querySelectorAll("button");
+
+for (const item of buttons) {
+      const collectedInteraction = item.onclick = () => {
+            if (item.id === "clear") {
+                  display.innerText = "";
+            } else if (item.id === "backspace") {
+                  /** @type {string} */
+                  let text = display.innerText.toString();
+                  display.innerText = text.substring(0, text.length - 1);
+            } else if (display.innerText != "" && item.id === "equal") {
+                  display.innerText = eval(display.innerText);
+            } else if (display.innerText === "" && item.id === "equal") {
+                  display.innerText = "Empty!";
+                  setTimeout(() => (display.innerText = ""), 2000);
+            } else {
+                  display.innerText += item.id;
+            }
+
+            return collectedInteraction;
+      };
+}
+
+const themeToggleButton = document.querySelector(".theme_toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggle_icon");
+
+let isDark = true;
+
+themeToggleButton.onclick = () => {
+      calculator.classList.toggle("dark");
+
+      themeToggleButton.classList.toggle("active");
+
+      isDark = !isDark;
+}
