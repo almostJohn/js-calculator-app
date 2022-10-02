17837772e0fe94afc1723ef399c0be69f9ce719a
almostJohn <garcia.johngale@gmail.com>
2022-10-02 17:41:54 +0800

    refactor buttons

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
+/** @format */
+
 const display = document.querySelector("#display");
 
 const buttons = document.querySelectorAll("button");
 
 for (const item of buttons) {
-      const collectedInteraction = item.onclick = () => {
-            if (item.id === "clear") {
-                  display.innerText = "";
-            } else if (item.id === "backspace") {
-                  /** @type {string} */
-                  let text = display.innerText.toString();
-                  display.innerText = text.substring(0, text.length - 1);
-            } else if (display.innerText != "" && item.id === "equal") {
-                  display.innerText = eval(display.innerText);
-            } else if (display.innerText === "" && item.id === "equal") {
-                  display.innerText = "Empty!";
-                  setTimeout(() => (display.innerText = ""), 2000);
-            } else {
-                  display.innerText += item.id;
-            }
+	const collectedInteraction = (item.onclick = () => {
+		if (item.id === "clear") {
+			display.innerText = "";
+		} else if (item.id === "backspace") {
+			/** @type {string} */
+			let text = display.innerText.toString();
+			display.innerText = text.substring(0, text.length - 1);
+		} else if (display.innerText !== "" && item.id === "equal") {
+			display.innerText = eval(display.innerText);
+		} else if (display.innerText === "" && item.id === "equal") {
+			display.innerText = "Empty!";
+			setTimeout(() => (display.innerText = ""), 2000);
+		} else {
+			display.innerText += item.id;
+		}
 
-            return collectedInteraction;
-      };
+		return collectedInteraction;
+	});
 }
 
 const themeToggleButton = document.querySelector(".theme_toggler");
@@ -30,9 +32,9 @@ const toggleIcon = document.querySelector(".toggle_icon");
 let isDark = true;
 
 themeToggleButton.onclick = () => {
-      calculator.classList.toggle("dark");
+	calculator.classList.toggle("dark");
 
-      themeToggleButton.classList.toggle("active");
+	themeToggleButton.classList.toggle("active");
 
-      isDark = !isDark;
-}
+	isDark = !isDark;
+};
